9313218e8d2ac95cd4ce88fff5f5a310ce6ac574
rmody3 <rmody3@gmail.com>
2017-02-08 20:50:05 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,26 @@ require 'pry'
 
 class String
 
+
   def sentence?
-    
+    self.punctuation_at_end?(".")
   end
 
   def question?
-
+    self.punctuation_at_end?("?")
   end
 
   def exclamation?
-
+    self.punctuation_at_end?("!")
   end
 
-  def count_sentences
+  def punctuation_at_end?(punctuation)
+    self.end_with?(punctuation)
+  end
 
+
+  def count_sentences
+    split_sentences = self.split(/[?.!]+/)
+    count = split_sentences.count
   end
 end
